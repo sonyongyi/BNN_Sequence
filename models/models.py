dbbfa826e0e2bc81a8627c7d4b393df7367ca528
sonyongyi <sonyongyi@kaist.ac.kr>
2020-09-14 15:54:56 +0900

    2020.09.14

--- models/models.py
+++ models/models.py
@@ -16,6 +16,61 @@ def seq_matrix(period,rotate):
     M_Mat=torch.from_numpy(M_Matrix)
     return M_Mat
 
+
+class MLPBinaryConnect(nn.Module):
+    """Multi-Layer Perceptron used for MNIST. No convolution layers.
+       This model is the MLP architecture used in paper "An empirical study of Binary NN optimization".
+    """
+    def __init__(self, in_features, out_features, num_units=2048, momentum=0.15, eps=1e-4,drop_prob=0,batch_affine=True):
+        super(MLPBinaryConnect, self).__init__()
+        self.in_features = in_features
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+
+        self.dropout3 = nn.Dropout(p=drop_prob)
+
+        self.dropout4 = nn.Dropout(p=drop_prob)
+
+
+        self.fc1 = BinaryLinear(in_features, num_units, bias=False)
+        self.bn1 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
+
+        self.fc2 = BinaryLinear(num_units, num_units, bias=False)
+        self.bn2 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
+
+        self.fc3 = BinaryLinear(num_units, num_units, bias=False)
+        self.bn3 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
+        
+
+        self.fc4 = BinaryLinear(num_units, out_features, bias=False)
+        self.bn4 = nn.BatchNorm1d(out_features, eps=eps, momentum=momentum,affine=batch_affine)
+
+
+    def forward(self, x):
+        x = x.view(-1, self.in_features)
+        x = self.dropout1(x)
+        x = self.fc1(x)
+        x = F.relu(self.bn1(x))
+        x = self.dropout2(x)
+
+
+        x = self.fc2(x)
+        x = F.relu(self.bn2(x))
+        x = self.dropout3(x)
+
+
+        x = self.fc3(x)
+        x = F.relu(self.bn3(x))
+        x = self.dropout4(x)
+
+        x = self.fc4(x)
+        x = self.bn4(x)
+
+        return x  # if used NLL loss, the output should be changed to F.log_softmax(x,dim=1),
+    
+    
 class MLPBinaryConnect_M1(nn.Module):
     """Multi-Layer Perceptron used for MNIST. No convolution layers.
        This model is the MLP architecture used in paper "An empirical study of Binary NN optimization".
@@ -44,8 +99,9 @@ class MLPBinaryConnect_M1(nn.Module):
         self.bn3 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
         
         self.mseq = BinaryLinear(num_units, out_features,bias=False)
-        self.mseq.requires_grad_(False)
         self.mseq.weight.copy_(self.seq_data)
+        #self.mseq.requires_grad_(False)
+        
 
         self.fc4 = BinaryLinear(num_units, out_features, bias=False)
         self.bn4 = nn.BatchNorm1d(out_features, eps=eps, momentum=momentum,affine=batch_affine)
