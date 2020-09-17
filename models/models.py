cc94f075cd4d6f97af7db5fed38e19cafd6dcf01
sonyongyi <sonyongyi@kaist.ac.kr>
2020-09-17 15:58:14 +0900

    2020.09.17 15:58

--- models/models.py
+++ models/models.py
@@ -8,7 +8,7 @@ from .binarize_modules import *
 
 def seq_matrix(period,rotate):
     M_seq = np.array(max_len_seq(period)[0])
-    M_seq = np.concatenate([M_seq,M_seq[:1]])
+    #M_seq = np.concatenate([M_seq,M_seq[:0]])
     M_Matrix=np.copy(M_seq)
     for i in range(rotate-1):
         M_seq=np.roll(M_seq,1)
@@ -78,7 +78,7 @@ class MLPBinaryConnect_M1(nn.Module):
     def __init__(self, in_features, out_features, num_units=2048, momentum=0.15, eps=1e-4,drop_prob=0,batch_affine=True):
         super(MLPBinaryConnect_M1, self).__init__()
         self.in_features = in_features
-        self.seq_data = seq_matrix(11,out_features) #test line
+        
         self.dropout1 = nn.Dropout(p=drop_prob)
 
         self.dropout2 = nn.Dropout(p=drop_prob)
@@ -95,16 +95,17 @@ class MLPBinaryConnect_M1(nn.Module):
         self.fc2 = BinaryLinear(num_units, num_units, bias=False)
         self.bn2 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
 
-        self.fc3 = BinaryLinear(num_units, num_units, bias=False)
-        self.bn3 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
+        self.fc3 = BinaryLinear(num_units, num_units-1, bias=False)
+        self.bn3 = nn.BatchNorm1d(num_units-1, eps=eps, momentum=momentum,affine=batch_affine)
         
-        self.mseq = BinaryLinear(num_units, out_features,bias=False)
+        self.seq_data = seq_matrix(11,out_features) #test line
+        self.mseq = BinaryLinear(num_units-1, out_features,bias=False)
         self.mseq.requires_grad_(False)
         self.mseq.weight.copy_(self.seq_data)
         
         
 
-        self.fc4 = BinaryLinear(num_units, out_features, bias=False)
+        #self.fc4 = BinaryLinear(num_units, out_features, bias=False)
         self.bn4 = nn.BatchNorm1d(out_features, eps=eps, momentum=momentum,affine=batch_affine)
 
 
@@ -142,23 +143,21 @@ class MLPBinaryConnect_M2(nn.Module):
 
         self.dropout2 = nn.Dropout(p=drop_prob)
         
-        self.seq_data = seq_matrix(11,num_units)
-        
         self.dropout3 = nn.Dropout(p=drop_prob)
 
         self.dropout4 = nn.Dropout(p=drop_prob)
 
         
-        
+        self.seq_data = seq_matrix(11,num_units)
         self.mseq = nn.Linear(num_units, num_units,bias=False)
         self.mseq.requires_grad_(False)
         self.mseq.weight.copy_(self.seq_data)
         
         
-        self.fc1 = BinaryLinear(in_features, num_units, bias=False)
-        self.bn1 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
+        self.fc1 = BinaryLinear(in_features, num_units-1, bias=False)
+        self.bn1 = nn.BatchNorm1d(num_units-1, eps=eps, momentum=momentum,affine=batch_affine)
 
-        self.fc2 = BinaryLinear(num_units, num_units, bias=False)
+        #self.fc2 = BinaryLinear(num_units, num_units, bias=False)
         self.bn2 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
 
         self.fc3 = BinaryLinear(num_units, num_units, bias=False)
@@ -194,24 +193,29 @@ class MLPBinaryConnect_M2(nn.Module):
 
         return x  # if used NLL loss, the output should be changed to F.log_softmax(x,dim=1),
     
+
 class MLPBinaryConnect_M3(nn.Module):
     """Multi-Layer Perceptron used for MNIST. No convolution layers.
        This model is the MLP architecture used in paper "An empirical study of Binary NN optimization".
     """
-    def __init__(self, in_features, out_features, num_units=2048, momentum=0.15, eps=1e-4,drop_prob=0,batch_affine=True):
-        super(MLPBinaryConnect_M1, self).__init__()
+    def __init__(self, in_features, out_features, num_units=2048, momentum=0.5, eps=1e-4,drop_prob=0,batch_affine=True):
+        super(MLPBinaryConnect_M3, self).__init__()
         self.in_features = in_features
-        self.seq_data = torch. ones(num_units, num_units, requires_grad=False) #test line
         self.dropout1 = nn.Dropout(p=drop_prob)
 
         self.dropout2 = nn.Dropout(p=drop_prob)
-
-
+        
         self.dropout3 = nn.Dropout(p=drop_prob)
 
         self.dropout4 = nn.Dropout(p=drop_prob)
 
-
+        '''
+        self.seq_data = seq_matrix(11,num_units)
+        self.mseq = nn.Linear(num_units, num_units,bias=False)
+        self.mseq.requires_grad_(False)
+        self.mseq.weight.copy_(self.seq_data)
+        '''
+        
         self.fc1 = BinaryLinear(in_features, num_units, bias=False)
         self.bn1 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
 
@@ -221,9 +225,7 @@ class MLPBinaryConnect_M3(nn.Module):
         self.fc3 = BinaryLinear(num_units, num_units, bias=False)
         self.bn3 = nn.BatchNorm1d(num_units, eps=eps, momentum=momentum,affine=batch_affine)
         
-        self.mseq = nn.Linear(num_units, num_units,bias=False)
-        self.mseq.requires_grad_(False)
-        self.mseq.weight.copy_(self.seq_data)
+        
 
         self.fc4 = BinaryLinear(num_units, out_features, bias=False)
         self.bn4 = nn.BatchNorm1d(out_features, eps=eps, momentum=momentum,affine=batch_affine)
@@ -246,13 +248,12 @@ class MLPBinaryConnect_M3(nn.Module):
         x = F.relu(self.bn3(x))
         x = self.dropout4(x)
 
-        x = self.mseq(x)
+        
 
         x = self.fc4(x)
         x = self.bn4(x)
 
         return x  # if used NLL loss, the output should be changed to F.log_softmax(x,dim=1),
-    
     
 class VGGBinaryConnect(nn.Module):
     """VGG-like net used for Cifar10.
@@ -355,15 +356,20 @@ class VGGBinaryConnect_M1(nn.Module):
         self.conv6 = BinConv2d(512, 512, kernel_size=3, padding=1,bias=False)
         self.bn6 = nn.BatchNorm2d(512, eps=eps, momentum=momentum,affine=batch_affine)
 
-
+        
+        self.mseq = nn.Linear(13, out_features,bias=False)
+        self.mseq.requires_grad_(False)
+        self.mseq.weight.copy_(self.seq_data)
+        
+        
         self.fc1 = BinaryLinear(512 * 4 * 4, 1024, bias=False)
         self.bn7 = nn.BatchNorm1d(1024,affine=batch_affine)
 
-        self.fc2 = BinaryLinear(1024, 1024, bias=False)
-        self.bn8 = nn.BatchNorm1d(1024,affine=batch_affine)
+        self.fc2 = BinaryLinear(1024, 1023, bias=False)
+        self.bn8 = nn.BatchNorm1d(1023,affine=batch_affine)
 
 
-        self.fc3 = BinaryLinear(1024, out_features, bias=False)
+        self.fc3 = BinaryLinear(1023, out_features, bias=False)
         self.bn9 = nn.BatchNorm1d(out_features,affine=batch_affine)
 
     def forward(self, x):
